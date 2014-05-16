2e55f0594e29219ba62be83673f85a56c13634db
huweiqing <huweiqing915@gmail.com>
2014-05-16 14:44:12 +0800

    增加文档id-位置

--- Include/PageLib.h
+++ Include/PageLib.h
@@ -20,6 +20,9 @@
 #include "Config.h"
 #include "EncodingConverter.h"
 
+#define LIB_PATH "/home/hwq/src/mini_search_engine/Data/ripepage.lib"
+#define OFFSET_PATH "/home/hwq/src/mini_search_engine/Data/offset.lib"
+
 class PageLib {
 public:
 	PageLib();
@@ -28,8 +31,9 @@ public:
 	void traversal_dir(const std::string &dirname);
 	//title - content
 	void get_title_content(const std::string &filename, std::string &, std::string &);
-	//docid	- url - title - content	
-	void write_lib(int, const std::string&, const std::string&, std::string&);
+	//docid	- url - title - content	- offset - size
+	void write_lib(int, const std::string&, const std::string&, std::string&, int &, int &);
+	void build_offset_index(int docid, int offset, int length);
 
 private:
 };
