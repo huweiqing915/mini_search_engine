f7ab2f59ea9d1e4dea39865016667970233bbdfb
huweiqing <huweiqing915@gmail.com>
2014-05-16 19:20:29 +0800

    “增加去重”

--- Include/DelDuplicate.h
+++ Include/DelDuplicate.h
@@ -5,25 +5,7 @@
 	> Created Time: 2014年05月15日 星期四 21时38分12秒
  ************************************************************************/
 //网页去重，指纹法
+//每个网页都有一个优先级队列
 #ifndef DEL_DUPLICATE_H_
 #define DEL_DUPLICATE_H_
 
-#include <string>
-#include <map>
-#include <vector>
-#include <queue>
-
-struct PageWord {
-	int _docid;
-	std::string _word;
-	int _frequency;
-};
-
-class DelDuplicate {
-public:
-private:
-	std::priority_queue<PageWord, std::vector<PageWord>, compare> _word_queue;
-	std::set<std::string> _exclude;
-};
-
-#endif
---
+++ Include/PageSegment.h
@@ -0,0 +1,50 @@
+/*************************************************************************
+	> File Name: PageSegment.h
+	> Author: huwq
+	> Mail:huweiqing915@gmail.com 
+	> Created Time: 2014年05月16日 星期五 15时12分36秒
+ ************************************************************************/
+//把网页切词，统计词频，并产生topk
+
+#ifndef PAGE_SEGMENT_H_
+#define PAGE_SEGMENT_H_
+
+#include <string>
+#include <vector>
+#include <queue>
+#include <map>
+#include <string.h>
+
+#include "MixSegment.hpp"
+
+struct PageWord {
+	std::string _word;
+	int _frequency;
+	PageWord(const std::string &word, int frequency):_word(word), _frequency(frequency)
+	{
+		//construct
+	}
+};
+
+struct compare {	//提供给优先级队列priority_queue的比较方法
+	bool operator()(const PageWord &a, const PageWord &b)
+	{
+		return a._frequency < b._frequency;
+	}
+};
+
+class PageSegment {
+public:
+	PageSegment();
+	~PageSegment();
+	void build_word_queue(char *web_page);
+	std::string get_top_word();
+private:
+	std::priority_queue<PageWord, std::vector<PageWord>, compare> _word_queue;
+	std::set<std::string> _exclude;
+	void init_path(std::string &dict_path, std::string &model_path);
+};
+
+#endif
+
+
